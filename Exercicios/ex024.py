d5f72d37ea8638ba4046ba7e62c7270726822a7d
Renato Bastos Pope <renato@pope.med.br>
2020-10-17 15:51:31 -0300

    atualizado os exercícios até o 27

--- Exercicios/ex024.py
+++ Exercicios/ex024.py
@@ -1,7 +1,2 @@
-# Exercício criado no VS do windows
-#Testando se funciona no PyCharm do Mac
-nome = str(input('Qual é o seu nome? '))
-print('Muito prazer te conhecer {}'.format(nome))
-#Corrigi alguns erros no VS do Windows.
-#No PyCharm do Windows eu edito, mas não tem interpretador.
-print('Lembrar de assistir um vídeo em como adicionar um interpretetador sem estragar o projeto.')
+cid = str(input('Qual a cidade você nasceu? ')).strip()
+print(cid[:5].upper() == 'SANTO')
