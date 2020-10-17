281c3761091de45c0d93901688b4fd695b04b13d
Renato Bastos Pope <renato@pope.med.br>
2020-10-17 19:21:12 -0300

    exercicios corrigidos.

---
+++ Exercicios/ex035.py
@@ -0,0 +1,14 @@
+print('==='*8)
+print('\033[7mANALISADOR DE TRIANGULO\033[m')
+print('==='*8)
+x = float(input('Digite o primeiro lado do triangulo: '))
+y = float(input('Digite o segundo lado do triangulo: '))
+z = float(input('Digite o terceiro lado do triangulo: '))
+if x >= y + z or y >= z + x or z >= x + y:
+    print('Essas medidas \033[1;31mnão\033[m formam um triangulo!')
+else:
+    print('As medidas dadas formam um triangulo.')
+if x == y and x == z:
+    print('É um triangulo equilatero.')
+if x == y or x == z or y == z:
+    print('É um triangulo isosceles.')
