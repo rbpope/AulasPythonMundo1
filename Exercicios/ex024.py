7f7a82a2d9f689d127bd578bdb930f485105533a
Renato Bastos Pope <renato@pope.med.br>
2020-10-17 10:23:52 -0300

    Update ex024.py
    Corrigi a str e o erro de português dentro da string.

--- Exercicios/ex024.py
+++ Exercicios/ex024.py
@@ -1,4 +1,5 @@
 # Exercício criado no VS do windows
 #Testando se funciona no PyCharm do Mac
-nome = srt(input('Qual é o seu nome? '))
-print('Muito prazer te comnhecer {}'.format(nome))
+nome = str(input('Qual é o seu nome? '))
+print('Muito prazer te conhecer {}'.format(nome))
+#Corrigi alguns erros no VS do Windows.
