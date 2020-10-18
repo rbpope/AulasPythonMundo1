b479ee0fcf277aa24fd3ee1f48a590b3d0ac612f
Renato Bastos Pope <renato@pope.med.br>
2020-10-18 19:06:27 -0300

    atualizado

--- Exercicios/ex007.py
+++ Exercicios/ex007.py
@@ -1,18 +1,4 @@
-nome = str(input('Como é o nome do aluno: '))
 n1 = float(input('Digite a primeira nota do aluno: '))
 n2 = float(input('Digite a segunda nota do aluno: '))
-n3 = float(input('Digite a terceira nota do aluno: '))
-m = float((n1 + n2 + n3) / 3)
-ma = float(7.0)
-if m >= ma:
-    print('='*50)
-    print('{}, sua média foi {:.2f} e você está Aprovado, Boas Férias!!!'.format(nome, m))
-    print('='* 50)
-if float(5.0) <= m < ma:
-    print('=' * 50)
-    print('{}, sua média foi {:.2f}. Vamos fazer recuperação!!!'.format(nome,m))
-    print('='* 50)
-if  m < float(5.0):
-    print('=' * 50)
-    print('{}, sua média foi {:.2f} e você foi Reprovado!!!'.format(nome, m))
-    print('='* 50)
+m = float((n1 + n2) / 2)
+print('A sua média foi {}'.format(m))
